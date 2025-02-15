6c71006384caa78f446e77f9d26cf116a035545c
sajjad_alinia <sajjad.alinia1398@gmail.com>
2025-02-15 19:33:30 +0330

    fix: :wrench: update layout dashboard

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,28 @@
+import { Outlet } from "react-router-dom";
+
 const DashboardLayout = () => {
-  return <div className="w-svw h-svh bg-slate-100">DashboardLayout</div>;
+  return (
+    <div className="w-full h-screen bg-slate-100">
+      <Navbar />
+      <div className="flex w-full h-full">
+        <Sidebar />
+        <main className="p-3">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
+
+const Navbar = () => {
+  const fullname = localStorage.getItem("fullname");
+  return <div className="w-full shadow-md p-3">{fullname}</div>;
+};
+
+const Sidebar = () => {
+  return (
+    <div className="w-2/12 h-full border-l border-slate-300 p-3">asdfasdf</div>
+  );
+};
